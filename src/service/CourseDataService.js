e59580e7f628c2158d87b135b074ae7b530676d9
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-12-28 00:04:27 +0700

    add CRUD feature to service

--- src/service/CourseDataService.js
+++ src/service/CourseDataService.js
@@ -6,7 +6,28 @@ const INSTRUCTOR_API_URL = `${COURSE_API_URL}/instructors/${INSTRUCTOR}`
 
 class CourseDataService {
     retrieveAllCourses(name) {
+        //console.log('executed service')
         return axios.get(`${INSTRUCTOR_API_URL}/courses`);
+    }
+
+    retrieveCourse(name, id) {
+        //console.log('executed service')
+        return axios.get(`${INSTRUCTOR_API_URL}/courses/${id}`);
+    }
+
+    deleteCourse(name, id) {
+        //console.log('executed service')
+        return axios.delete(`${INSTRUCTOR_API_URL}/courses/${id}`);
+    }
+
+    updateCourse(name, id, course) {
+        //console.log('executed service')
+        return axios.put(`${INSTRUCTOR_API_URL}/couses/${id}`, course);
+    }
+
+    createCourse(name, course) {
+        //console.log('executed service')
+        return axios.post(`${INSTRUCTOR_API_URL}/courses/`, course)
     }
 }
 
