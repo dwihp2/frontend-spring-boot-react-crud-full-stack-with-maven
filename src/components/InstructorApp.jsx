9a66563f84a7c529620e1d953afa7049e077b4f5
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-12-27 21:16:34 +0700

    call ListComponent on app and add service backend

--- src/components/InstructorApp.jsx
+++ src/components/InstructorApp.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import ListCoursesComponent from "./ListCoursesComponent";
 
 class InstructorApp extends Component {
     render() {
-        return (
+        return (<>
             <h1>Instructor Application</h1>
+            <ListCoursesComponent />
+            </>
         )
     }
 }
