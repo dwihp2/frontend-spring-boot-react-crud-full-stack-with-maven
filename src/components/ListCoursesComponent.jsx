176f1d56b6e3b64d820d543e4127140dec8038a8
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-12-27 21:26:54 +0700

    change ListCoursesComponent to based on props

--- src/components/ListCoursesComponent.jsx
+++ src/components/ListCoursesComponent.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
+import CourseDataService from '../service/CourseDataService'
 
+const INSTRUCTOR = 'in28minutes'
 
 class ListCoursesComponent extends Component {
-    render() {
-        return(
-            <div className="container">
-                <h3>All Courses</h3>
-                <div className="container">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Learn full stack with Spring Boot and Angular</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+    
+    constructor(props) {
+        super(props)
+        this.refreshCourses = this.refreshCourses.bind(this)
+    }
+
+    componentDidMount() {
+        this.refreshCourses();
+    }
+
+    refreshCourses() {
+        CourseDataService.retrieveAllCourses(INSTRUCTOR)    //hardcoded
+        .then(
+            response=> {
+                console.log(response);
+            }
         )
     }
 }
